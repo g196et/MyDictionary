a21eca6a1b9d5e4b4c3bae9dccdef207f33343ff
Konstantin Malakhov <g196et@gmail.com>
2016-06-12 01:33:11 +0400

    init

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary
+    {
+        //Константы для хэширования
+        const int size = 26;
+        const int randomConst = 1568;
+
+        string[] hashList;
+
+        public MyDictionary()
+        {
+            hashList = new string[50000];
+        }
+
+        /// <summary>
+        /// Метод хэширования
+        /// </summary>
+        /// <param name="key">ключ, который будет хэшироваться</param>
+        /// <returns></returns>
+        int GetHash(string key)
+        {
+            int hash = 0;
+            foreach (char c in key)
+            {
+                hash = (hash * size + c) % randomConst;
+            }
+            return hash;
+        }
+
+        /// <summary>
+        /// Метод добавления нового элемента
+        /// </summary>
+        /// <param name="key">ключ</param>
+        /// <param name="value">значение</param>
+        /// <returns>true - если удалось добавить, иначе false</returns>
+        public bool Insert(string key, string value)
+        {
+            int hash = GetHash(key);
+            //Если нашли с таким же хэшем, значит что-то не так
+            if (Find(key) == true)
+                return false;
+            hashList[hash] = value;
+            return true;
+        }
+
+        /// <summary>
+        /// Метод поиска по ключу
+        /// </summary>
+        /// <param name="key">ключ, по которому производится поиск</param>
+        /// <returns>true - если удалось найди, иначе false</returns>
+        public bool Find(string key) 
+        {
+            int hash = GetHash(key);
+            if (hashList[hash] == null)
+                return false;
+            else return true;
+        }
+
+        /// <summary>
+        /// Метод удаления по ключу
+        /// </summary>
+        /// <param name="key">ключ, по которому производится удаление</param>
+        /// <returns>true - если удалось удалить, иначе false</returns>
+        public bool Remove(string key)
+        {
+            int hash = GetHash(key);
+            if (Find(key) == false)
+                return false;
+            hashList[hash] = null;
+            return true;
+        }
+
+        public void Print()
+        {
+            foreach (string tmp in hashList)
+                if (tmp != null)
+                    Console.Write(tmp + " ");
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string enter, key, value;
+            MyDictionary myDictionary = new MyDictionary();
+            Console.Write("Insert, Find, Remove, Print, Exit\n> ");
+            enter = Console.ReadLine();
+            while (enter != "Exit")
+            {     
+                switch (enter)
+                {
+                    case ("Insert"):
+                        Console.Write("Key = ");
+                        key = Console.ReadLine();
+                        Console.Write("Value = ");
+                        value = Console.ReadLine();
+                        if (!myDictionary.Insert(key, value))
+                            Console.WriteLine("Error");
+                        else Console.WriteLine("Item added");
+                        break;
+                    case ("Find"):
+                        Console.Write("Key = ");
+                        key = Console.ReadLine();
+                        if (myDictionary.Find(key))
+                            Console.WriteLine("Item is exist");
+                        else Console.WriteLine("Item not found");
+                        break;
+                    case ("Remove"):
+                        Console.Write("Key = ");
+                        key = Console.ReadLine();
+                        if (myDictionary.Remove(key))
+                            Console.WriteLine("Item is remove");
+                        else Console.WriteLine("Item not found");
+                        break;
+                    case ("Print"):
+                        myDictionary.Print();
+                        break;
+                }
+                Console.Write("> ");
+                enter = Console.ReadLine();
+            }
+        }
+    }
+}
